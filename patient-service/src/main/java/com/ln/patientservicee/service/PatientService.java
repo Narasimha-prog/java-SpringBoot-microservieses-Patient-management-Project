ff5144ffa6e3418700c33d7f5e802050dacec20a
narasimhaproj <lnreddyreddy405@gmail.com>
2025-05-20 19:10:24 +0530

    adding another module

--- patient-service/src/main/java/com/ln/patientservicee/service/PatientService.java
+++ patient-service/src/main/java/com/ln/patientservicee/service/PatientService.java
@@ -4,9 +4,12 @@ import com.ln.patientservicee.dto.PatientRequestDTO;
 import com.ln.patientservicee.dto.PatientResponseDTO;
 import com.ln.patientservicee.excption.EmailAlreadyExistedException;
 import com.ln.patientservicee.excption.PatientNotFoundException;
+import com.ln.patientservicee.grpc.BillingServiceGrpcClient;
+import com.ln.patientservicee.kafka.KafkaProducer;
 import com.ln.patientservicee.mapper.PatientMapper;
 import com.ln.patientservicee.model.Patient;
 import com.ln.patientservicee.repository.IPatientRepository;
+
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -18,9 +21,13 @@ public class PatientService implements IPatientService {
 
 
     private final IPatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final KafkaProducer kafkaProducer;
 
-    public PatientService(IPatientRepository patientRepository) {
+    public PatientService(IPatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient, KafkaProducer kafkaProducer) {
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.kafkaProducer = kafkaProducer;
     }
 
     @Override
@@ -35,13 +42,16 @@ public class PatientService implements IPatientService {
             throw new EmailAlreadyExistedException("A Patient with this Email is already existed " + requestDTO.getPatientEmail());
         }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(requestDTO));
+        billingServiceGrpcClient.createBillingAccount(newPatient.getPid().toString(), newPatient.getPatientName(), newPatient.getPatientEmail());
+
+        kafkaProducer.sendEvent(newPatient);
         return PatientMapper.toDTO(newPatient);
     }
 
     @Override
     public PatientResponseDTO updatePatient(UUID patientId, PatientRequestDTO requestDTO) {
         Patient patient = patientRepository.findById(patientId).orElseThrow(() -> new PatientNotFoundException("Patient is not found with this ID: " + patientId));
-        if (patientRepository.existsByPatientEmailAndPidNot(requestDTO.getPatientEmail(),patientId)) {
+        if (patientRepository.existsByPatientEmailAndPidNot(requestDTO.getPatientEmail(), patientId)) {
             throw new EmailAlreadyExistedException("A Patient with this Email is already existed " + requestDTO.getPatientEmail());
         }
         //once a patient is registered with date cannot be changed so ,No update of Registered date
@@ -49,7 +59,7 @@ public class PatientService implements IPatientService {
         patient.setPatientEmail(requestDTO.getPatientEmail());
         patient.setPatientAddress(requestDTO.getPatientAddress());
         patient.setPatientDateOfBirth(LocalDate.parse(requestDTO.getPatientDateOfBirth()));
-     Patient updatedPatient=   patientRepository.save(patient);
+        Patient updatedPatient = patientRepository.save(patient);
         return PatientMapper.toDTO(updatedPatient);
     }
 
@@ -60,7 +70,7 @@ public class PatientService implements IPatientService {
 
     @Override
     public PatientResponseDTO getPatientById(UUID patientId) {
-      Patient patient= patientRepository.findById(patientId).orElseThrow(() -> new PatientNotFoundException("Patient is not found with this ID: " + patientId));
+        Patient patient = patientRepository.findById(patientId).orElseThrow(() -> new PatientNotFoundException("Patient is not found with this ID: " + patientId));
         return PatientMapper.toDTO(patient);
     }
 }
